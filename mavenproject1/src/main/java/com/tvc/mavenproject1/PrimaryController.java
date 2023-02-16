5d81f91c882608c942f3d3e766ecfa1bd8bcef04
hoangvbm.2808 <cuongdepzaizxcv@gmail.com.com>
2023-02-16 10:50:49 +0700

    edit

--- mavenproject1/src/main/java/com/tvc/mavenproject1/PrimaryController.java
+++ mavenproject1/src/main/java/com/tvc/mavenproject1/PrimaryController.java
@@ -8,7 +8,6 @@ public class PrimaryController {
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
-        int t =1;
-        if(t>0) {t=2;}
+        if (true) return;
     }
 }
